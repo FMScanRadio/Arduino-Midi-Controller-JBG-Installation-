5cdd865f4b2813ed97c0d167be81815b977a0060
Kiril Michael Kanartcheff <kiril.kmk@gmail.com>
2015-08-25 03:41:37 -0700

    Update pingmidi.ino

--- pingmidi.ino
+++ pingmidi.ino
@@ -1,4 +1,4 @@
-//Requires NewPing library for the Arduino code to function: http://playground.arduino.cc/Code/NewPing
+//Requires NewPing library: http://playground.arduino.cc/Code/NewPing
 #include <NewPing.h>
 
 #define ECHO_PIN_1     2
@@ -9,44 +9,34 @@
 #define TRIGGER_PIN_3  7
 #define ECHO_PIN_4     8
 #define TRIGGER_PIN_4  9
-#define MAX_DISTANCE 400
+#define MAX_DISTANCE 400 //In meters. Set max distance according to specs. 300m for SRF04, 400 for SRF05.
 
 NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
 NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
 NewPing sonar3(TRIGGER_PIN_3, ECHO_PIN_3, MAX_DISTANCE);
 NewPing sonar4(TRIGGER_PIN_4, ECHO_PIN_4, MAX_DISTANCE);
 
+//Set delay to minimum refresh time as per rangefinder specs. 50ms for SRF04/SRF05.
+int delayTime = 50; 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9200);
 }
 
 void loop() {
-  delay(50);
   int uS1 = sonar1.ping();
-  Serial.print("1 Ping: ");
-  Serial.print(uS1 / US_ROUNDTRIP_CM);
-  Serial.println("cm");
   Serial.write(uS1 / US_ROUNDTRIP_CM);
+  delay(delayTime); //Full delay after each ping to make sure last radio wave has dissipated before the next ping.
   
-  delay(50);
   int uS2 = sonar2.ping();
-  Serial.print("2 Ping: ");
-  Serial.print(uS2 / US_ROUNDTRIP_CM);
-  Serial.println("cm");
   Serial.write(uS2 / US_ROUNDTRIP_CM);
+  delay(delayTime);
   
-  delay(50);
   int uS3 = sonar3.ping();
-  Serial.print("3 Ping: ");
-  Serial.print(uS3 / US_ROUNDTRIP_CM);
-  Serial.println("cm");
   Serial.write(uS3 / US_ROUNDTRIP_CM);
+  delay(delayTime);
   
-  delay(50);
   int uS4 = sonar4.ping();
-  Serial.print("4 Ping: ");
-  Serial.print(uS4 / US_ROUNDTRIP_CM);
-  Serial.println("cm");
   Serial.write(uS4 / US_ROUNDTRIP_CM);
+  delay(delayTime);
 }
