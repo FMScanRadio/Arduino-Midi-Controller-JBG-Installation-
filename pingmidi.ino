8532c7de4e47f5233616aaae5e23391d53f08e58
Kiril Michael Kanartcheff <kiril.kmk@gmail.com>
2015-09-14 13:15:24 -0700

    Update pingmidi.ino

--- pingmidi.ino
+++ pingmidi.ino
@@ -7,36 +7,78 @@
 #define TRIGGER_PIN_2  5
 #define ECHO_PIN_3     6
 #define TRIGGER_PIN_3  7
-#define ECHO_PIN_4     8
-#define TRIGGER_PIN_4  9
-#define MAX_DISTANCE 400 //In centimeters. Set max distance according to specs. 300cm for SRF04, 400cm for SRF05.
+#define MAX_DISTANCE 400 //In meters. Set to within max distance of sensor (300m for SRF04, 400 for SRF05.)
 
 NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
 NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
 NewPing sonar3(TRIGGER_PIN_3, ECHO_PIN_3, MAX_DISTANCE);
-NewPing sonar4(TRIGGER_PIN_4, ECHO_PIN_4, MAX_DISTANCE);
 
 //Set delay to minimum refresh time as per rangefinder specs. 50ms for SRF04/SRF05.
 int delayTime = 50; 
+
+int total1 = 0;
+int total2 = 0;
+int total3 = 0;
+
+int old1 = 0;
+int old2 = 0;
+int old3 = 0;
+
+int fade = MAX_DISTANCE * 0.05;
 
 void setup() {
   Serial.begin(9200);
 }
 
 void loop() {
+  //Each sensor pings once in a 50ms*N (N being # of sensors) cycle.
+  //If there is a response (!= 0), then save and send the detected range.
+  //If there is no response (== 0), then if an old save value exists, fade and send it.
+  //If there is no response (== 0) and no old save value, send the max distance value.
+  
   int uS1 = sonar1.ping();
-  Serial.write(uS1 / US_ROUNDTRIP_CM);
+  total1 = uS1 / US_ROUNDTRIP_CM;
+  if (total1 != 0) {
+  Serial.write(total1);
+  old1 = total1;
+  } else {
+    if (old1 < MAX_DISTANCE){
+    old1 = old1 + fade;
+    Serial.write(old1);
+    }else {
+    Serial.write(MAX_DISTANCE);
+  }
+  }
+  
   delay(delayTime); //Full delay after each ping to make sure last radio wave has dissipated before the next ping.
   
   int uS2 = sonar2.ping();
-  Serial.write(uS2 / US_ROUNDTRIP_CM);
+  total2 = uS2 / US_ROUNDTRIP_CM;
+  if (total2 != 0) {
+  Serial.write(total2);
+  old2 = total2;
+  } else {
+    if (old2 < MAX_DISTANCE){
+    old2 = old2 + fade;
+    Serial.write(old2);
+    }else {
+    Serial.write(MAX_DISTANCE);
+  }
+  }
   delay(delayTime);
   
   int uS3 = sonar3.ping();
-  Serial.write(uS3 / US_ROUNDTRIP_CM);
-  delay(delayTime);
-  
-  int uS4 = sonar4.ping();
-  Serial.write(uS4 / US_ROUNDTRIP_CM);
+  total3 = uS3 / US_ROUNDTRIP_CM;
+  if (total3 != 0) {
+  Serial.write(total3);
+  old3 = total3;
+  } else {
+    if (old3 < MAX_DISTANCE){
+    old3 = old3 + fade;
+    Serial.write(old3);
+    }else {
+    Serial.write(MAX_DISTANCE);
+  }
+  }
   delay(delayTime);
 }
